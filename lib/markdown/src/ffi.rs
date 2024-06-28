693d7eafd82c799148e236e814f637b22734f3d0
timofey_arkhipov <timar@hotmail.com>
2024-06-28 14:42:04 +0300

    refactor(markdown): get rid of `libc`

--- lib/markdown/src/ffi.rs
+++ lib/markdown/src/ffi.rs
@@ -1,10 +1,9 @@
 use std::ffi::{CStr, CString};
-use libc::c_char;
 
 use crate::Markdown;
 
 #[no_mangle]
-pub extern "C" fn md_to_html_generate(s: *const c_char) -> *mut c_char {
+pub extern "C" fn md_to_html_generate(s: *const i8) -> *mut i8 {
     let cstr = unsafe { CStr::from_ptr(s).to_str().unwrap() };
     let compiled = Markdown::compile(cstr.to_string());
     CString::new(compiled).unwrap().into_raw()
