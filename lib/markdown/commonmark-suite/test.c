7a44d39028c7fd72608dd1b9e5cefc7971be913c
timofey_arkhipov <timar@hotmail.com>
2024-06-28 14:03:37 +0300

    refactor(markdown): remove `main` from `test.c` file

--- lib/markdown/commonmark-suite/test.c
+++ lib/markdown/commonmark-suite/test.c
@@ -6,20 +6,3 @@ extern char *md_to_html_generate(const char *c);
 void md_to_html_free(char *c) {
     free(c);
 }
-
-int main(void) {
-    const char* markdown = "# Hello from C!\nThis is *Markdown*.";
-
-    char* html = md_to_html_generate(markdown);
-
-    if (html != NULL) {
-        printf("%s\n", html);
-        md_to_html_free(html);
-    } else {
-        fprintf(stderr, "Markdown compilation failed\n");
-        return 1;
-    }
-
-    return 0;
-
-}
