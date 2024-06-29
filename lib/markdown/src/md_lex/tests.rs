13bb4a71ee0504cd8d196d4656f7801971084170
timofey_arkhipov <timar@hotmail.com>
2024-06-29 13:48:39 +0300

    test(markdown): added some `Cursor` tests

--- lib/markdown/src/md_lex/tests.rs
+++ lib/markdown/src/md_lex/tests.rs
@@ -3,9 +3,34 @@ mod tests {
     use crate::md_lex::Cursor;
 
     #[test]
+    fn consume_while() {
+        let mut cursor = Cursor::from_string("~~~~~~~");
+        cursor.consume_while("~");
+        assert_eq!(
+            cursor.current(),
+            None
+        );
+    }
+
+    #[test]
+    fn skip_n() {
+        let mut cursor = Cursor::from_string("asdf");
+        cursor.skip_n(100);
+        assert_eq!(
+            cursor.current(),
+            None
+        );
+    }
+
+    #[test]
+    fn match_curr() {
+        let mut cursor = Cursor::from_string("a");
+        assert!(cursor.match_curr("a"));
+    }
+
+    #[test]
     fn utf8_slice() {
         let mut cursor = Cursor::from_string("aὐv");
-        dbg!(cursor.len());
         assert_eq!(
             &cursor[1..2],
             "ὐ"
